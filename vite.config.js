769236d21a001af61bceaab2711f617d4160c2ea
wlz2022 <2667461198@qq.com>
2024-06-01 12:32:57 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === "production" ? "/testWebVue/" : "/"
+  base: process.env.NODE_ENV === "production" ? "/my_vite_vue3/" : "/"
 })
